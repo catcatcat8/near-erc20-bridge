70b19b82c679ccaf1aee67772e314425ad1d1259
catcatcat8 <lebedev666e@gmail.com>
2023-03-17 01:09:00 +0300

    storage deposit/withdraw fully tested

--- integration-tests/src/main.ava.ts
+++ integration-tests/src/main.ava.ts
@@ -1,17 +1,18 @@
 import { Worker, NearAccount, NEAR, BN } from 'near-workspaces'
 import anyTest, { TestFn } from 'ava'
 
-const ONE_NEAR = new BN(new BN("10").pow(new BN("24")))
+const ONE_NEAR = new BN(new BN('10').pow(new BN('24')))
+const DELTA = new BN(new BN('10').pow(new BN('22')))
 const STORAGE_BYTE_COST = '1.5 mN'
 
-const LIMIT_PER_SEND = 2000
+const LIMIT_PER_SEND = new BN(2000)
 const FEE_NUMERATOR = 8000
 const TOTAL_SUPPLY = '1000000000000000000000000'
 const RELAYER_ROLE = 'ed25519:6E8sCci9badyRkXb3JoRpBj5p8C6Tw41ELDZoiihKEtp'
 
-let payForRegister = new BN("0")
-let payForFtOnTransfer = new BN("0")
-let payForFulfill = new BN("0")
+let payForRegister = new BN('0')
+let payForFtOnTransfer = new BN('0')
+let payForFulfill = new BN('0')
 
 const test = anyTest as TestFn<{
   worker: Worker
@@ -30,6 +31,14 @@ async function registerUser(ft: NearAccount, user: NearAccount) {
     { account_id: user },
     { attachedDeposit: STORAGE_BYTE_COST },
   )
+}
+
+async function storageDeposit(
+  user: NearAccount,
+  bridge: NearAccount,
+  amount: BN,
+) {
+  await user.call(bridge, 'storage_deposit', {}, { attachedDeposit: amount })
 }
 
 test.beforeEach(async (t) => {
@@ -68,7 +77,9 @@ test.beforeEach(async (t) => {
   })
   await registerUser(token, bridge)
 
-  const storageData: any = await bridge.view('get_storage_paid_info', {user: bridge.accountId})
+  const storageData: any = await bridge.view('get_storage_paid_info', {
+    user: bridge.accountId,
+  })
   payForRegister = new BN(storageData[2])
   payForFtOnTransfer = new BN(storageData[3])
   payForFulfill = new BN(storageData[4])
@@ -93,8 +104,8 @@ test('Constructor', async (t) => {
     FEE_NUMERATOR,
     10000,
   ])
-  t.is(await bridge.view('get_limit_per_send', {}), LIMIT_PER_SEND)
-  t.is(await bridge.view('get_nonce', {}), '0')
+  t.is(await bridge.view('get_limit_per_send', {}), LIMIT_PER_SEND.toString())
+  t.is(await bridge.view('get_nonce', {}), new BN(0).toString())
   t.is(
     await bridge.view('get_transactions_by_user', { user: owner.accountId }),
     '[]',
@@ -118,11 +129,128 @@ test('Successful changes fee numerator by admin', async (t) => {
   )
 })
 
-test('Successful storage deposit', async (t) => {
-  const { owner, token, bridge, user } = t.context.accounts
-  await user.call(bridge, 'storage_deposit', {}, {attachedDeposit: ONE_NEAR})
+test('Successful first and second storage deposit', async (t) => {
+  const { bridge, user } = t.context.accounts
+
+  await storageDeposit(user, bridge, ONE_NEAR)
   const expectedStoragePaid = ONE_NEAR.sub(payForRegister)
-  const actualUserPaid = bridge.view("get_storage_paid_info", {user: user.accountId})
+  let actualUserPaid: any = await bridge.view('get_storage_paid_info', {
+    user: user.accountId,
+  })
   t.true(actualUserPaid[0])
-  t.is(actualUserPaid[1], expectedStoragePaid.toString())
+  t.deepEqual(actualUserPaid[1], expectedStoragePaid.toString())
+
+  await storageDeposit(user, bridge, ONE_NEAR)
+  actualUserPaid = await bridge.view('get_storage_paid_info', {
+    user: user.accountId,
+  })
+  t.true(actualUserPaid[0])
+  t.deepEqual(actualUserPaid[1], expectedStoragePaid.add(ONE_NEAR).toString())
+})
+
+test('First storage deposit fails if NEAR not enough', async (t) => {
+  const { bridge, user } = t.context.accounts
+
+  const error = await t.throwsAsync(
+    user.call(
+      bridge,
+      'storage_deposit',
+      {},
+      { attachedDeposit: payForRegister.sub(new BN(1)) },
+    ),
+  )
+  t.is(
+    panicMessageFromThrowsAsync(error),
+    'Smart contract panicked: Not enough NEAR attached',
+  )
+  await storageDeposit(user, bridge, payForRegister)
+  await storageDeposit(user, bridge, new BN(1))
+})
+
+test('Bridge balance is not reduced after storageDeposit()', async (t) => {
+  const { bridge, user } = t.context.accounts
+
+  const bridgeBalanceBefore = await (await bridge.balance()).available
+  await storageDeposit(user, bridge, payForRegister)
+  t.true(await (await bridge.balance()).available.gt(bridgeBalanceBefore))
+  t.true(
+    await (await bridge.balance()).available.sub(bridgeBalanceBefore).lt(DELTA),
+  ) // profit is small
+
+  const bridgeBalanceBefore2 = await (await bridge.balance()).available
+  await storageDeposit(user, bridge, ONE_NEAR)
+  t.true(
+    await (await bridge.balance()).available
+      .sub(ONE_NEAR)
+      .gt(bridgeBalanceBefore2),
+  )
+  t.true(
+    await (await bridge.balance()).available
+      .sub(bridgeBalanceBefore2)
+      .sub(ONE_NEAR)
+      .lt(DELTA),
+  )
+})
+
+test('Correct storage withdraw', async (t) => {
+  const { bridge, user } = t.context.accounts
+
+  let error = await t.throwsAsync(
+    user.call(bridge, 'storage_withdraw', {
+      amount: new BN(1).toString(),
+    }),
+  )
+  t.is(
+    panicMessageFromThrowsAsync(error),
+    'Smart contract panicked: No storage paid',
+  )
+
+  await storageDeposit(user, bridge, ONE_NEAR)
+
+  error = await t.throwsAsync(
+    user.call(bridge, 'storage_withdraw', {
+      amount: ONE_NEAR.sub(payForRegister).add(new BN(1)).toString(),
+    }),
+  )
+  t.is(
+    panicMessageFromThrowsAsync(error),
+    'Smart contract panicked: Amount is more than your storage paid',
+  )
+
+  const userBalanceBefore = await (await user.balance()).available
+  const storagePaidBefore = new BN(
+    ((await bridge.view('get_storage_paid_info', {
+      user: user.accountId,
+    })) as any)[1],
+  )
+  await user.call(bridge, 'storage_withdraw', {
+    amount: ONE_NEAR.sub(payForRegister).toString(),
+  })
+  t.true(
+    new BN(
+      ((await bridge.view('get_storage_paid_info', {
+        user: user.accountId,
+      })) as any)[1],
+    ).eq(storagePaidBefore.sub(ONE_NEAR.sub(payForRegister))),
+  )
+
+  // balance after ~ balance before + 1 NEAR
+  t.true(
+    await (await user.balance()).available
+      .sub(userBalanceBefore)
+      .gt(ONE_NEAR.sub(DELTA)),
+  )
+  t.true(
+    await (await user.balance()).available.sub(userBalanceBefore).lt(ONE_NEAR),
+  )
+
+  error = await t.throwsAsync(
+    user.call(bridge, 'storage_withdraw', {
+      amount: new BN(1).toString(),
+    }),
+  )
+  t.is(
+    panicMessageFromThrowsAsync(error),
+    'Smart contract panicked: Amount is more than your storage paid',
+  )
 })
